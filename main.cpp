1580b4b1bc42fbf738dadab52d78b46ba92f02dc
Cristian-Nastase <cristianemail@duck.com>
2026-03-17 17:12:16 +0200

    Restarted the project, finished the Box class

--- main.cpp
+++ main.cpp
@@ -1,16 +1,202 @@
-#include <cstring>
 #include <iostream>
+#include <map>
+#include <vector>
+
+class Box {
+private:
+    float* padding;
+    float* margin;
+    float border;
+
+    float* returnVector(float value);
+public:
+    Box();
+    Box(float* padding, float* margin, float border);
+    Box(const Box &obj);
+    Box& operator=(const Box &obj);
+    ~Box();
+
+    friend std::istream& operator>>(std::istream& in, Box& obj);
+    friend std::ostream& operator<<(std::ostream& out, const Box& obj);
+};
+
+float* Box::returnVector(float value) {
+    float* vector = new float[4];
+
+    for (int i = 0; i < 4; i++) {
+        vector[i] = value;
+    }
+
+    return vector;
+}
+
+Box::Box() {
+    padding = returnVector(1.0);
+    margin = returnVector(1.0);
+    border = 1.0;
+}
+
+Box::Box(float* padding, float* margin, float border) {
+    this->padding = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->padding[i] = padding[i];
+    }
+
+    this->margin = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->margin[i] = margin[i];
+    }
+
+    this->border = border;
+}
+
+Box::Box(const Box &obj) {
+    this->padding = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->padding[i] = obj.padding[i];
+    }
+
+    this->margin = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->margin[i] = obj.margin[i];
+    }
+
+    this->border = obj.border;
+}
+
+Box& Box::operator=(const Box &obj) {
+    if (this == &obj) {
+        return *this;
+    }
+
+    delete[] this->padding;
+    this->padding = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->padding[i] = obj.padding[i];
+    }
+
+    delete[] this->margin;
+    this->margin = returnVector(1.0);
+    for (int i = 0; i < 4; i++) {
+        this->margin[i] = obj.margin[i];
+    }
+
+    this->border = obj.border;
+
+    return *this;
+}
+
+Box::~Box() {
+    if (padding != nullptr) {
+        delete[] padding;
+        padding = nullptr;
+    }
+
+    if (margin != nullptr) {
+        delete[] margin;
+        margin = nullptr;
+    }
+}
+
+std::istream& operator>>(std::istream& in, Box& obj) {
+    std::cout<<"Padding (4 values): ";
+    for (int i = 0; i < 4; i++) {
+        in>>obj.padding[i];
+    }
+
+    std::cout<<"Margin (4 values): ";
+    for (int i = 0; i < 4; i++) {
+        in>>obj.margin[i];
+    }
+
+    std::cout<<"Border (1 value): ";
+    in>>obj.border;
+
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Box& obj) {
+    out<<"Padding: ";
+    for (int i = 0; i < 4; i++) {
+        out<<obj.padding[i]<<" ";
+    }
+
+    out<<"\n";
+
+    out<<"Margin: ";
+    for (int i = 0; i < 4; i++) {
+        out<<obj.margin[i]<<" ";
+    }
+
+    out<<"\n";
+    out<<"Border: "<<obj.border;
+
+    return out;
+}
+
+class Color {
+private:
+    static const std::map<char, char*> colorMap;
+    static const char* endString;
+    char currentColor;
+    bool active;
+public:
+    Color();
+    Color(char currentColor, bool active);
+    Color(const Color& obj);
+    Color& operator=(const Color& obj);
+    ~Color();
+
+    char getCurrentColor() const;
+    char* getEndString() const;
+
+    void printColored(const char* text) const;
+};
+
+Color::Color() {
+    colorMap.insert({"R", "Dsalut"});
+
+    currentColor = "W";
+    active = true;
+}
+
+void Color::printColored(const char* text) const {
+    if (!this->active)
+        return;
+
+    char key = this->getCurrentColor();
+    const char* color = colorMap.at(key);
+
+    std::cout<<color<<text<<endString;
+}
+
+char Color::getCurrentColor() const {
+    return this->currentColor;
+}
+
+char* Color::getEndString() const {
+    return this->endString;
+}
+
+class Element {
+private:
+    static int noElements;
+    char* text;
+    const char* className;
+    const char* id;
+    bool isColored;
+    Box& boxModel;
+    Color& color;
+public:
+
+};
 
 class Selector {
     private:
-        static int blocksNumber;
-        int* specificity;
         char* selectorString;
-        bool cascades;
-        bool compoundSelector;
-        int* returnNewSpecificity();
-        int* calculateSpecificity(const char *selectorString);
-        bool checkCompound(const char *selectorString);
+        int type;
+        std::vector<Element> element;
+        bool isEmpty;
     public:
         Selector();
         Selector(char* selectorString, bool cascades);
@@ -19,160 +205,15 @@ class Selector {
         ~Selector();
 };
 
-// 0. id 1. class 2. element
-int* returnNewSpecificity() {
-    int* specificity = new int[3];
-    for (int i = 0; i < 3; i++) {
-        specificity[i] = 0;
-    }
-
-    return specificity;
-}
-
-bool checkCompound(const char *selectorString) {
-    char *combinators = strcpy(new char[7], ":> ~+|");
-
-    for (int i = 0; i < strlen(combinators); i++) {
-        if (strchr(selectorString, combinators[i]) != nullptr) {
-            delete[] combinators;
-            combinators = nullptr;
-            return true;
-        }
-    }
-
-    delete[] combinators;
-    combinators = nullptr;
-
-    return false;
-}
-
-int* calculateSpecificity(const char *selectorString) {
-    int* specificity = returnNewSpecificity();
-
-    bool lastCharSpace;
-    for (int i = 0; i < strlen(selectorString); i++) {
-        switch (selectorString[i]) {
-            case '#':
-                specificity[0]++;
-                break;
-            case '.':
-                specificity[1]++;
-                break;
-            case ' ':
-                lastCharSpace = true;
-                break;
-            default:
-                if (!lastCharSpace)
-                    break;
-
-                specificity[2]++;
-                lastCharSpace = false;
-        }
-    }
-
-    return specificity;
-}
-
-Selector::Selector() {
-    blocksNumber++;
-    selectorString = strcpy(new char[2], "*");
-    cascades = true;
-    compoundSelector = false;
-    specificity = returnNewSpecificity();
-}
-
-Selector::Selector(char* selectorString, bool cascades) {
-    blocksNumber++;
-    this->selectorString = strcpy(new char[strlen(selectorString) + 1], selectorString);
-    this-> cascades = cascades;
-    specificity = calculateSpecificity(selectorString);
-    compoundSelector = checkCompound(selectorString);
-}
-
-Selector::Selector(const Selector &obj) {
-    blocksNumber++;
-    selectorString = strcpy(new char[strlen(obj.selectorString) + 1], obj.selectorString);
-    cascades = obj.cascades;
-    compoundSelector = obj.compoundSelector;
-    specificity = new int[3];
-    for (int i = 0; i < 3; i++) {
-        specificity[i] = obj.specificity[i];
-    }
-}
-
-Selector& Selector::operator=(const Selector &obj) {
-    if (this == &obj)
-        return *this;
-
-    delete[] selectorString;
-    selectorString = strcpy(new char[strlen(obj.selectorString) + 1], selectorString);
-
-    cascades = obj.cascades;
-    compoundSelector = obj.compoundSelector;
-
-    delete[] specificity;
-    specificity = new int[3];
-    for (int i = 0; i < 3; i++) {
-        specificity[i] = obj.specificity[i];
-    }
-
-    return *this;
-}
-
-Selector::~Selector() {
-    delete[] specificity;
-    delete[] selectorString;
-}
-
-class Property {
-    private:
-        static double totalNumber;
-        const int line;
-        char* name;
-        bool affectsRendering;
-        int valuesAcceptedNumber;
-    public:
-        Property();
-        Property(char* name, bool affectsRendering);
-        Property(char* name, bool affectsRendering, int valuesAcceptedNumber);
-        Property(const Property &property);
-        Property& operator=(const Property &property);
-        ~Property();
-};
-
-class Value {
-    private:
-        float* numericValues;
-        char valueType;
-        char* measureOfUnit;
-        bool multipleValues;
-    public:
-    Value();
-    Value(float* numericValues, char valueType);
-    Value(float* numericValues, char valueType, char* measureOfUnit);
-    Value(float* numericValues, char valueType, char* measureOfUnit, bool multipleValues);
-    Value(const Value& value);
-    Value& operator=(const Value& value);
-    ~Value();
-};
-
-class Function {
-    private:
-        static int totalNumber;
-        const int id;
-        const bool inLine;
-        char* name;
-        int argumentsAccepted;
-        char* returnType;
-    public:
-        Function();
-        Function(char* name);
-        Function(char* name, int argumentsAccepted, char* returnType);
-        Function(char* name, bool inLine, int argumentsAccepted, char* returnType);
-        Function(const Function &function);
-        Function& operator=(const Function &function);
-        ~Function();
-};
-
 int main() {
+    float padding[4], margin[4];
+    for (int i = 0; i < 4; i++) {
+        padding[i] = 1.2;
+        margin[i] = 2.1;
+    }
+
+    float border = 2.5;
+    Box a(padding, margin, border), b = a, c;
+    c = a;
+    std::cout<<"\033[33m"<<&a<<" "<<&b<<" "<<&c;
 }
