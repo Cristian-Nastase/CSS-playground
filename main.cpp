5e7d4e655ef9c4743edff716b1919006f9a6bcd1
Cristian-Nastase <cristianemail@duck.com>
2026-03-19 22:46:34 +0200

    Added default methods to color and element classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstring>
 
+// box class
+
 class Box {
 private:
     float* padding;
@@ -70,14 +72,10 @@ Box& Box::operator=(const Box &obj) {
         return *this;
     }
 
-    delete[] this->padding;
-    this->padding = returnVector(1.0);
     for (int i = 0; i < 4; i++) {
         this->padding[i] = obj.padding[i];
     }
 
-    delete[] this->margin;
-    this->margin = returnVector(1.0);
     for (int i = 0; i < 4; i++) {
         this->margin[i] = obj.margin[i];
     }
@@ -135,10 +133,13 @@ std::ostream& operator<<(std::ostream& out, const Box& obj) {
     return out;
 }
 
+
+// color class
+
+
 class Color {
 private:
-    static const std::map<char, char*> colorMap;
-    static char* endString;
+    // static const std::map<char, char*> colorMap;
     char currentColor;
     bool active;
 public:
@@ -148,34 +149,92 @@ public:
     Color& operator=(const Color& obj);
     ~Color();
 
-    char getCurrentColor() const;
-    char* getEndString() const;
+    friend std::istream& operator>>(std::istream& in, Color& obj);
+    friend std::ostream& operator<<(std::ostream& out, const Color& obj);
 
-    void printColored(const char* text) const;
+    char getCurrentColor() const;
+    void setCurrentColor(char color);
+    void resetColor();
+
+    void printColored(const char* text);
+
 };
 
-Color::Color() {;
-    currentColor = 'W';
-    active = true;
+void Color::resetColor() {
+    std::cout<<"\033[0m";
 }
 
-void Color::printColored(const char* text) const {
+void Color::printColored(const char* text) {
     if (!this->active)
         return;
 
     char key = this->getCurrentColor();
-    const char* color = colorMap.at(key);
+    // const char* color = colorMap.at(key);
 
-    std::cout<<color<<text<<endString;
+    // std::cout<<color<<text;
+    this->resetColor();
 }
 
 char Color::getCurrentColor() const {
     return this->currentColor;
 }
 
-char* Color::getEndString() const {
-    return endString;
+void Color::setCurrentColor(char color) {
+    this->currentColor = color;
 }
+
+Color::Color() {;
+    currentColor = 'W';
+    active = true;
+}
+
+Color::Color(char currentColor, bool active) {
+    this->currentColor = currentColor;
+    this->active = active;
+}
+
+Color::Color(const Color& obj) {
+    this->currentColor = obj.currentColor;
+    this->active = obj.active;
+}
+
+Color& Color::operator=(const Color& obj) {
+    if (this == &obj) {
+        return *this;
+    }
+
+    this->currentColor = obj.currentColor;
+    this->active = obj.active;
+
+    return *this;
+}
+
+Color::~Color() {
+    return;
+}
+
+std::istream& operator>>(std::istream& in, Color& obj) {
+    std::cout<<"Select a color: ";
+
+    int option;
+
+    in>>option;
+
+    if (option == 1) {
+        obj.setCurrentColor('R');
+    }
+
+    return in;
+}
+
+
+std::ostream& operator<<(std::ostream& out, const Color& obj) {
+    out<<obj.getCurrentColor();
+    return out;
+}
+
+// element class
+
 
 class Element {
 private:
@@ -184,17 +243,21 @@ private:
     char* className;
     char* id;
     bool isColored;
-    const Box* boxModel;
-    const Color* color;
+    Box* boxModel;
+    Color* color;
 
     char* returnEmptyString();
     char* testEmptyString(char* text);
+    void readString(std::istream& in, std::string text, char*& member);
 public:
     Element();
     Element(char* text, char* className, char* id, bool isColored);
     Element(const Element& obj);
     Element& operator=(const Element& obj);
     ~Element();
+
+    friend std::istream& operator>>(std::istream& in, Element& obj);
+    friend std::ostream& operator<<(std::ostream& out, const Element& obj);
 };
 
 int Element::noElements = 0;
@@ -211,7 +274,9 @@ char* Element::testEmptyString(char* text) {
         return returnEmptyString();
     }
 
-    return text;
+    char* copy = new char[strlen(text) + 1];
+    strcpy(copy, text);
+    return copy;
 }
 
 Element::Element() : boxModel(new Box), color(new Color) {
@@ -234,16 +299,16 @@ Element::Element(char* text, char* className, char* id, bool isColored) : boxMod
     this->isColored = isColored;
 }
 
-Element::Element(const Element& obj) : boxModel(new Box), color(new Color) {
+Element::Element(const Element& obj) : boxModel(new Box(*obj.boxModel)), color(new Color(*obj.color)) {
     noElements++;
 
-    this->text = new char[strlen(obj.text)];
+    this->text = new char[strlen(obj.text) + 1];
     strcpy(this->text, obj.text);
 
-    this->className = new char[strlen(obj.className)];
+    this->className = new char[strlen(obj.className) + 1];
     strcpy(this->className, obj.className);
 
-    this->id = new char[strlen(obj.id)];
+    this->id = new char[strlen(obj.id) + 1];
     strcpy(this->id, obj.id);
 
     this->isColored = obj.isColored;
@@ -255,33 +320,73 @@ Element& Element::operator=(const Element& obj) {
     }
 
     delete[] this->className;
-    this->className = new char[strlen(obj.className)];
+    this->className = new char[strlen(obj.className) + 1];
     strcpy(this->className, obj.className);
 
     delete[] this->id;
-    this->id = new char[strlen(obj.id)];
+    this->id = new char[strlen(obj.id) + 1];
     strcpy(this->id, obj.id);
 
     delete[] this->text;
-    this->text = new char[strlen(obj.text)];
+    this->text = new char[strlen(obj.text) + 1];
     strcpy(this->text, obj.text);
 
     this->isColored = obj.isColored;
 
     delete boxModel;
-    this->boxModel = obj.boxModel;
+    this->boxModel = new Box(*obj.boxModel);
 
     delete color;
-    this->color = obj.color;
+    this->color = new Color(*obj.color);
+
+    return *this;
 }
 
 Element::~Element() {
+    noElements--;
     delete boxModel;
     delete color;
     delete[] className;
     delete[] id;
     delete[] text;
 }
+
+void Element::readString(std::istream& in, std::string textToPrint, char*& member) {
+    char *buffer = new char[256];
+    std::cout<<textToPrint;
+    in>>buffer;
+
+    delete[] member;
+    member = new char[strlen(buffer) + 1];
+    strcpy(member, buffer);
+
+    delete[] buffer;
+}
+
+std::istream& operator>>(std::istream& in, Element& obj) {
+    obj.readString(in,"Enter a text: ", obj.text);
+    obj.readString(in, "Enter a class name: ", obj.className);
+    obj.readString(in, "Enter an id: ", obj.id);
+
+    in>>*obj.boxModel;
+    in>>*obj.color;
+
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, const Element &obj) {
+    out<<"Text: "<<obj.text<<"\n";
+    out<<"Class Name: "<<obj.className<<"\n";
+    out<<"Id:"<<obj.id<<"\n";
+
+    out<<"Box model: "<<*obj.boxModel<<"\n";
+    out<<"Color: "<<*obj.color;
+
+    return out;
+}
+
+// selector class
+
 
 class Selector {
     private:
@@ -298,14 +403,8 @@ class Selector {
 };
 
 int main() {
-    float padding[4], margin[4];
-    for (int i = 0; i < 4; i++) {
-        padding[i] = 1.2;
-        margin[i] = 2.1;
-    }
-
-    float border = 2.5;
-    Box a(padding, margin, border), b = a, c;
-    c = a;
-    std::cout<<"\033[33m"<<&a<<" "<<&b<<" "<<&c;
+    Element e;
+    std::cin>>e;
+    std::cout<<e;
+    return 0;
 }
